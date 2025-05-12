2a0ac7d426d94c31cf62f2ed96b9ea39c5513e55
ujjwalthapa404 <thapaujjwal20@gmail.com>
2025-05-12 21:28:39 +0545

    Updated BookMetaServices.cs

--- ADGroupCW/Services/Interface/IBookMetaSevice.cs
+++ ADGroupCW/Services/Interface/IBookMetaSevice.cs
@@ -5,19 +5,20 @@ namespace ADGroupCW.Services.Interfaces
 {
     public interface IBookMetaService
     {
-        //  Genre
+    
+        //Genre section manages the various formats in which books can be published
         Task<List<GenreReadDto>> GetGenresAsync();
         Task<GenreReadDto> AddGenreAsync(NameOnlyDto dto);
 
-        //  Publisher
+        //Publisher section retrieves a list of all registered publishers.
         Task<List<PublisherReadDto>> GetPublishersAsync();
         Task<PublisherReadDto> AddPublisherAsync(NameOnlyDto dto);
 
-        //  Award
+        //Award section retrieves a list of all registered book awards.
         Task<List<BookAwardReadDto>> GetAwardsAsync();
         Task<BookAwardReadDto> AddAwardAsync(NameOnlyDto dto);
 
-        // Format
+        // Format section Retrieves a list of all supported book formats
         Task<List<BookFormatReadDto>> GetFormatsAsync();
         Task<BookFormatReadDto> AddFormatAsync(NameOnlyDto dto);
     }
