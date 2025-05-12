e90b3fabfdacfc8e7d5e2cd12080181df9d49a0b
ujjwalthapa404 <thapaujjwal20@gmail.com>
2025-05-13 00:00:26 +0545

    updated AdminNavbar

--- Frontend/frontend/src/components/AdminNavbar.jsx
+++ Frontend/frontend/src/components/AdminNavbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/AdminDashboard.css';
+import '../styles/AdminDashboard.css';//importing css to admin dashboard
 
 const AdminNavbar = () => {
   return (
